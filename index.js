a840c5e10e92274eae646641ec532f7fc72352a1
JosiasPires <antonio.josias09@aluno.ifce.edu.br>
2022-11-14 21:15:04 -0300

    create playRound function

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 function getComputerChoice() {
-    const options = ['Rock', 'Paper', 'Scissors']
-    const computerChoice = options[Math.floor(Math.random() * options.length)]
-    return computerChoice
+    const options = ['ROCK', 'PAPER', 'SCISSORS'];
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    if (playerSelection === 'ROCK') {
+        if (computerSelection === 'ROCK') {
+            let result = "It's a tie! Rock doesn't beat Rock";
+        }
+        else if (computerSelection === 'PAPER') {
+            let result = "You lose! Paper beats Rock";
+        }
+        else if (computerSelection === 'SCISSORS') {
+            let result = "You win! Rock beats Scissors";
+        }
+    }
+    if (playerSelection === 'PAPER') {
+        if (computerSelection === 'ROCK') {
+            let result = "You win! Paper beats Rock";
+        }
+        else if (computerSelection === 'PAPER') {
+            let result = "It's a tie! Paper doesn't beat Paper";
+        }
+        else if (computerSelection === 'SCISSORS') {
+            let result = "You lose! Scissors beats Paper";
+        }
+    }
+    if (playerSelection === 'SCISSORS') {
+        if (computerSelection === 'ROCK') {
+            let result = "You lose! Rock beats Scissors";
+        }
+        else if (computerSelection === 'PAPER') {
+            let result = "You win! Scissors beats Paper";
+        }
+        else if (computerSelection === 'SCISSORS') {
+            let result = "It's a tie! Scissors doesn't beat Scissors";
+        }
+    }
+    return result;
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
